67814d50a609eb283fafe2de5e5a0a7603504b16
Julia Frances <104040854+jrf343@users.noreply.github.com>
2023-02-25 20:26:09 -0500

    wrote completed testing for shipFactory.js factory function

---
+++ tests/shipFactory.test.js
@@ -0,0 +1,60 @@
+const module = require('../src/factories/shipFactory');
+
+it('creates a new carrier with correct parameters', () => {
+    const ship = module.shipFactory('carrier');
+    expect(ship.type).toBe('carrier');
+    expect(ship.length).toBe(5);
+    expect(ship.hits.value).toBe(0);
+})
+
+it('creates a new battleship with correct parameters', () => {
+    const ship = module.shipFactory('battleship');
+    expect(ship.type).toBe('battleship');
+    expect(ship.length).toBe(4);
+    expect(ship.hits.value).toBe(0);
+})
+
+it('creates a new submarine with correct parameters', () => {
+    const ship = module.shipFactory('submarine');
+    expect(ship.type).toBe('submarine');
+    expect(ship.length).toBe(3);
+    expect(ship.hits.value).toBe(0);
+})
+
+it('creates a new cruiser with correct parameters', () => {
+    const ship = module.shipFactory('cruiser');
+    expect(ship.type).toBe('cruiser');
+    expect(ship.length).toBe(3);
+    expect(ship.hits.value).toBe(0);
+})
+
+it('creates a new patrolBoat with correct parameters', () => {
+    const ship = module.shipFactory('patrolBoat');
+    expect(ship.type).toBe('patrolBoat');
+    expect(ship.length).toBe(2);
+    expect(ship.hits.value).toBe(0);
+})
+
+it('takes a hit', () => {
+    const ship = module.shipFactory('carrier');
+    ship.hit();
+    expect(ship.hits.value).toBe(1);
+})
+
+it('takes multiple hits', () => {
+    const ship = module.shipFactory('carrier');
+    ship.hit();
+    ship.hit();
+    expect(ship.hits.value).toBe(2);
+})
+
+it('sinks correctly', () => {
+    const ship = module.shipFactory('carrier');
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+})
+
