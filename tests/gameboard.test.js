355198d885e1c848e1cd51e6ef82b9cdba7ebb5c
Julia Frances <104040854+jrf343@users.noreply.github.com>
2023-02-26 14:51:40 -0500

    finished testing gameboard factory

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,127 +1,51 @@
-const module = require('../src/factories/gameboard');
+const module = require("../src/factories/gameboard");
 
-it('generates a new gameboard', () => {
-    const board = module.gameboardFactory();
-    expect(board.getBoard()).toEqual( [
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ],
-        [
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' },
-          { occupied: false, miss: false, ship: '' }
-        ]
-      ]);
-})
+it("generates a new gameboard", () => {
+  const board = module.gameboardFactory();
+  expect(
+    board.getBoard
+      .flat()
+      .every(
+        (x) => x === { occupied: false, miss: false, shot: false, shipName: "" }
+      )
+  );
+});
+
+it("places ships correctly", () => {
+  const board = module.gameboardFactory();
+  board.placeShip("patrolBoat", 0, 0, "west");
+  expect(board.getBoard[0][0]).toEqual({
+    occupied: true,
+    miss: false,
+    shot: false,
+    shipName: "patrolBoat",
+  });
+  expect(board.getBoard[0][1]).toEqual({
+    occupied: true,
+    miss: false,
+    shot: false,
+    shipName: "patrolBoat",
+  });
+});
+
+it("doesn't place ships incorrectly", () => {
+  const board = module.gameboardFactory();
+  board.placeShip("patrolBoat", 0, 0, "west");
+  expect(() => {
+    board.placeShip("carrier", 0, 0, "north");
+  }).toThrow("Impossible placement: overlapping ships");
+});
+
+it("doesn't place ships incorrectly pt 2", () => {
+  const board = module.gameboardFactory();
+  expect(() => {
+    board.placeShip("carrier", 0, 0, "south");
+  }).toThrow("Impossible placement: off board");
+});
+
+it("recieves attacks correctly", () => {
+  const board = module.gameboardFactory();
+  board.placeShip("patrolBoat", 0, 0, "west");
+  board.recieveAttack(0, 0);
+  expect(board.getBoard[0][0].shot).toBe(true);
+});
