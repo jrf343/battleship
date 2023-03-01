30b57abebd91dc898f4f18fbe717127c07f2e68f
Julia Frances <104040854+jrf343@users.noreply.github.com>
2023-03-01 11:20:17 -0500

    set up index as entry point for script source

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    ship: "./src/factories/ship.js",
-    gameboard: "./src/factories/gameboard.js",
-    player: "./src/factories/player.js",
-    ai: "./src/factories/ai.js",
+    index: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
